6bcdefb0bdac5d91b726998bdf3bdd7ebe94750d
Hemant Yadav <yadavhemant1002@gmail.com>
2024-08-16 15:29:04 +0530

    Java first Program

---
+++ Aptech.java
@@ -0,0 +1,11 @@
+import java.util.*;//Must be used to take inputs.
+class input{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);//for taking inputs.
+        String name = sc.nextLine();//Here nextline is used to print the whole input including spaces.
+        //nextInt()
+        //nextFloat()
+        System.out.println(name);
+
+    }
+}
