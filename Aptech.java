542e4106d7e0abf02ff5d39ac1733e6e805d1581
Hemant Yadav <yadavhemant1002@gmail.com>
2024-08-18 12:07:34 +0530

    Triangle project.java

--- Aptech.java
+++ Aptech.java
@@ -1,11 +1,33 @@
-import java.util.*;//Must be used to take inputs.
-class input{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);//for taking inputs.
-        String name = sc.nextLine();//Here nextline is used to print the whole input including spaces.
-        //nextInt()
-        //nextFloat()
-        System.out.println(name);
+import java.util.Scanner;
 
-    }
+public class Triangle {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+        //Taking input from the user...
+		System.out.println("Enter 1st side of triangle: ");
+		int s1 = sc.nextInt();
+		System.out.println("Enter 2nd side of triangle: ");
+		int s2 = sc.nextInt();
+		System.out.println("Enter 3rd side of triangle: ");
+		int s3 = sc.nextInt();
+        //checking for the types of the triangle....
+		if (s1 == s2 ) {
+			if (s2 == s3) {
+				System.out.println("Equilateral");
+			}
+			else {
+				System.out.println("Isosceles");
+			}
+		}
+		else if (s2==s3 || s1==s3) {
+			System.out.println("Isosceles");
+		}
+        // Exception for invalid input ..... using basic knowledge of triangles...
+		else if (s1+s2<=s3 || s2+s3<=s1 || s1+s3<=s2) {
+			System.out.println("Invalid triangle");
+		}
+		else {
+			System.out.println("Scalene");
+		}
+		}
 }
